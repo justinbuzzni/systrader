6f0e32a182c49612dee6415acf827b7005a6e434
ForceCore <ssanzing@gmail.com>
2019-09-01 11:40:17 +0900

    Move restful server app scripts into restful

--- restful/run_7am.bat
+++ restful/run_7am.bat
@@ -1,5 +1,7 @@
 rem You need to copy, modify example_activate.bat as activate.bat for this script
 
+cd C:\Users\jdj\work\systrader
+
 echo "Server script"
 call activate.bat
 python wait7am.py
--- restful/wait7am.py
+++ restful/wait7am.py
@@ -2,7 +2,7 @@ import pause
 from datetime import datetime
 
 now = datetime.now()
-dt = datetime(now.year, now.month, now.day, 6, 50)
+dt = datetime(now.year, now.month, now.day, 6, 55)
 print("Time is now", now)
 print("Pausing until", dt)
 pause.until(dt)
