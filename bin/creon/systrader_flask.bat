086671234726637f5f2e13013aea05f7f4272b3a
quantylab <quantylab@gmail.com>
2021-04-24 09:30:42 +0900

    스크립트 수정

--- bin/creon/systrader_flask.bat
+++ bin/creon/systrader_flask.bat
@@ -1,3 +1,3 @@
 call C:\Users\%username%\Anaconda3x86\Scripts\activate.bat
 set FLASK_ENV=development
-python C:\Users\%username%\systrader\bridge_flask.py
+python C:\Users\%username%\systrader\quantylab\systrader\creon\bridge_flask.py
