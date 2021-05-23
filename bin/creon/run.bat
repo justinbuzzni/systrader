9223f2d710e8a3a20e8753dbd36724dfb7dc5517
quantylab <quantylab@gmail.com>
2021-05-23 15:40:14 +0900

    run, kill 스크립트 추가

--- bin/creon/run.bat
+++ bin/creon/run.bat
@@ -1,5 +1,5 @@
 call C:\Users\quantylab\Anaconda3x86\Scripts\activate.bat
-python C:\Users\quantylab\systrader\kill.py
+python C:\Users\quantylab\systrader\bin\creon\kill.py
 python C:\Users\quantylab\systrader\quantylab\systrader\creon\_creon.py disconnect
 python C:\Users\quantylab\systrader\quantylab\systrader\creon\_creon.py connect --id=%1 --pwd=%2 --pwdcert=%3
 python C:\Users\quantylab\systrader\manage.py runserver 0.0.0.0:8000 --noreload
