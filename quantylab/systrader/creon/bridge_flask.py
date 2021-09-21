206d55ee86e3244d07f3d53361d106da82d4c839
justin <justin@buzzni.com>
2021-09-21 16:00:01 +0900

    creon 에 있는 함수들 bridge server 호출 받을 수 있게 함

--- quantylab/systrader/creon/bridge_flask.py
+++ quantylab/systrader/creon/bridge_flask.py
@@ -1,6 +1,11 @@
+# from quantylab.systrader.creon import constants
+# from quantylab.systrader.creon import Creon
+import constants as constants
+from _creon import Creon
+
 from flask import Flask, request, jsonify
-from quantylab.systrader.creon import Creon
-from quantylab.systrader.creon import constants
+import sys
+sys.path.append("\\VBOXSVR\workspace\systrader")
 
 
 app = Flask(__name__)
@@ -51,13 +56,15 @@ def handle_stockcandles():
     c.wait()
     stockcode = request.args.get('code')
     n = request.args.get('n')
+    unit = request.args.get('unit', 'D')
     if n:
         n = int(n)
     date_from = request.args.get('date_from')
     date_to = request.args.get('date_to')
     if not (n or date_from):
         return 'Need to provide "n" or "date_from" argument.', 400
-    stockcandles = c.get_chart(stockcode, target='A', unit='D', n=n, date_from=date_from, date_to=date_to)
+    stockcandles = c.get_chart(
+        stockcode, target='A', unit=unit, n=n, date_from=date_from, date_to=date_to)
     return jsonify(stockcandles)
 
 
@@ -80,7 +87,8 @@ def handle_marketcandles():
         return [], 400
     if not (n or date_from):
         return '', 400
-    marketcandles = c.get_chart(marketcode, target='U', unit='D', n=n, date_from=date_from, date_to=date_to)
+    marketcandles = c.get_chart(
+        marketcode, target='U', unit='D', n=n, date_from=date_from, date_to=date_to)
     return jsonify(marketcandles)
 
 
@@ -120,5 +128,98 @@ def handle_investorbuysell():
     return jsonify(investorbuysell)
 
 
+@app.route('/get_balance', methods=['GET'])
+def handle_get_balance():
+    c.wait()
+    return jsonify(c.get_balance())
+
+
+@app.route('/get_remain_balance', methods=['GET'])
+def handle_get_remain_balance():
+    c.wait()
+    return jsonify(c.get_account_balance())
+
+
+@app.route('/buy', methods=['GET'])
+def handle_buy():
+    c.wait()
+    stockcode = request.args.get('code')
+    amount = request.args.get('amount')
+
+    return jsonify(c.buy(stockcode, amount))
+
+
+@app.route('/sell', methods=['GET'])
+def handle_sell():
+    c.wait()
+    stockcode = request.args.get('code')
+    amount = request.args.get('amount')
+    if amount:
+        amount = int(amount)
+
+    return jsonify(c.sell(stockcode, amount))
+
+
+@app.route('/holdingstocks', methods=['GET'])
+def handle_holdingstocks():
+    c.wait()
+    return jsonify(c.get_holdings())
+
+
+@app.route('/get_trade_history', methods=['GET'])
+def handle_get_trade_history():
+    c.wait()
+    return jsonify(c.get_trade_history())
+
+
+@app.route('/get_marketcap', methods=['GET'])
+def handle_get_marketcap():
+    c.wait()
+    return jsonify(c.get_marketcap())
+
+
+@app.route('/get_investorbuysell', methods=['GET'])
+def handle_get_investorbuysell():
+    c.wait()
+    stockcode = request.args.get('code')
+    n = request.args.get('n', None)
+    if n:
+        n = int(n)
+
+    return jsonify(c.get_investorbuysell(stockcode, n=n))
+
+
+@app.route('/get_shortstockselling', methods=['GET'])
+def handle_get_shortstockselling():
+    c.wait()
+    stockcode = request.args.get('code')
+    n = request.args.get('n', None)
+    if n:
+        n = int(n)
+
+    return jsonify(c.get_shortstockselling(stockcode, n=n))
+
+
+@app.route('/get_stockfeatures', methods=['GET'])
+def handle_get_stockfeatures():
+    c.wait()
+    stockcode = request.args.get('code')
+    return jsonify(c.get_stockfeatures(stockcode))
+
+
+@app.route('/get_stockstatus', methods=['GET'])
+def handle_get_stockstatus():
+    c.wait()
+    stockcode = request.args.get('code')
+    return jsonify(c.get_stockstatus(stockcode))
+
+
+@app.route('/get_stockcodes', methods=['GET'])
+def handle_get_stockcodes():
+    c.wait()
+    code = request.args.get('code')
+    return jsonify(c.get_stockcodes(code))
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
